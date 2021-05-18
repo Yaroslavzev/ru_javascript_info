9124c36ca8addd59eeff53131ea90d3ac0136994
Alexey Yaroslavtsev <yaroslavzevalex@gmail.com>
2021-05-18 10:17:50 +0300

    Data types: arrays

--- 5-data-types/04_array/file.js
+++ 5-data-types/04_array/file.js
@@ -32,10 +32,75 @@
 // Вызов arr[2]() синтаксически – старый добрый obj[method](), в роли obj – arr, а в роли method – 2.
 // Итак, у нас есть вызов функции arr[2] как метода объекта. Соответственно, он получает в качестве this объект arr и выводит массив:
 
-let arr = ["a", "b"];
+// let arr = ["a", "b"];
+//
+// arr.push(function() {
+//     alert( this );
+// })
+//
+// arr[2](); // "a","b",function
 
-arr.push(function() {
-    alert( this );
-})
+// Напишите функцию sumInput(), которая:
+// Просит пользователя ввести значения, используя prompt и сохраняет их в массив.
+// Заканчивает запрашивать значения, когда пользователь введёт не числовое значение, пустую строку или нажмёт «Отмена».
+// Подсчитывает и возвращает сумму элементов массива.
+// P.S. Ноль 0 – считается числом, не останавливайте ввод значений при вводе «0».
 
-arr[2](); // "a","b",function
+// function sumInput() {
+//     let array = []
+//
+//     while ( true ) {
+//         let symbol = prompt('введите число', 0);
+//
+//         if (symbol === "" || symbol === null || !isFinite(symbol)) break;
+//
+//         array.push(+symbol);
+//     }
+//
+//     alert(`количество элементов: ${array.length}, сумма элементов: ${array.reduce(function(acc, val) { return acc + val; }, 0)}`)
+//
+// }
+//
+// sumInput()
+
+// На входе массив чисел, например: arr = [1, -2, 3, 4, -9, 6].
+// Задача: найти непрерывный подмассив в arr, сумма элементов в котором максимальна.
+// Функция getMaxSubSum(arr) должна возвращать эту сумму.
+// Например:
+//
+// getMaxSubSum([-1, 2, 3, -9]) = 5 (сумма выделенных)
+// getMaxSubSum([2, -1, 2, 3, -9]) = 5
+// getMaxSubSum([-1, 2, 3, -9, 11]) = 11
+// getMaxSubSum([-2, -1, 1, 2]) = 3
+// getMaxSubSum([100, -9, 2, -3, 5]) = 100
+// getMaxSubSum([1, 2, 3]) = 6 (берём все)
+// Если все элементы отрицательные – ничего не берём(подмассив пустой) и сумма равна «0»:
+//
+// getMaxSubSum([-1, -2, -3]) = 0
+
+function getMaxSubSum(incoming_array) {
+    let sum_arrays = [];
+    let continuous_array_index = 0;
+
+    for ( let [i, value] of incoming_array.entries() ) {
+        if (value >= 0) {
+            sum_arrays[continuous_array_index] = (typeof sum_arrays[continuous_array_index] === 'undefined') ? value : sum_arrays[continuous_array_index] + value;
+        }
+        else if (incoming_array[i-1] >= 0) {
+            continuous_array_index += 1;
+        }
+        else {
+
+        }
+    }
+
+    console.log(sum_arrays)
+    alert(Math.max(...sum_arrays))
+}
+
+getMaxSubSum([-1, 2, 3, -9])
+getMaxSubSum([2, -1, 2, 3, -9])
+getMaxSubSum([-1, 2, 3, -9, 11])
+getMaxSubSum([-2, -1, 1, 2])
+getMaxSubSum([100, -9, 2, -3, 5])
+getMaxSubSum([1, 2, 3])
